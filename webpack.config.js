cfb3864c5c6398be36b3f2ce738c88c4db31f2a4
Patrik Freij <patrik.t.freij@gmail.com>
2017-11-05 16:41:14 +0100

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,22 @@ module.exports= {
 		rules: [
 			{
 				test: /\.css$/,
+				exclude: /(node_modules|bower_components)/,
 				use: [
 					'style-loader',
 					'css-loader'
 				]
 			},
 			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					'babel-loader'
+				]
+			},
+			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				exclude: /(node_modules|bower_components)/,
 				use: [
 					'file-loader'
 				]
