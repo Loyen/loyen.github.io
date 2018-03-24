9ab1949c43cf50f5d38c191a981fcd1d18ff9783
Patrik Freij <patrik.t.freij@gmail.com>
2018-03-24 13:25:37 +0100

    Add button component

---
+++ src/templates/button/button.vue
@@ -0,0 +1,49 @@
+<template>
+	<a class="button" :class="[ 'button--'+size ]" :href="href">
+		<c-icon v-if="icon" :name="icon" />
+		<span>{{ value }}</span>
+	</a>
+</template>
+
+<script>
+import icon from '../icon/icon.vue';
+
+export default {
+	components: {
+		"c-icon": icon
+	},
+	props: {
+		"title": { required: true },
+		"href": { required: true },
+		"icon": { required: false },
+		"size": { required: false, default: "m" },
+		"value": { required: false }
+	}
+}
+</script>
+
+<style>
+.button {
+	display: inline-block;
+}
+
+.button--xxxl {
+	font-size: 4em;
+}
+
+.button--xxl {
+	font-size: 2em;
+}
+
+.button--xl {
+	font-size: 1.5em;
+}
+
+.button--l {
+	font-size: 1.17em;
+}
+
+.button--s {
+	font-size: 0.83em;
+}
+</style>
