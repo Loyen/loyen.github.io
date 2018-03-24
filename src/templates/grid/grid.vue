5771f84324375cbc325a979fef72c3f7864cc633
Patrik Freij <patrik.t.freij@gmail.com>
2018-03-24 13:26:19 +0100

    Add grid components

---
+++ src/templates/grid/grid.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="grid" :style="{'grid-template': template, 'grid-gap': gap}">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		"template": { required: false, defaults: "none" },
+		"gap": { required: false, defaults: "none" }
+	}
+}
+</script>
+
+<style>
+.grid {
+	display: grid;
+}
+</style>
---
+++ src/templates/grid/item.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="grid-item" :style="{
+		'grid-row-start': rowStart,
+		'grid-row-end': rowEnd,
+		'grid-col-start': colStart,
+		'grid-col-end': colEnd
+	}">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		"rowStart": { required: false, defaults: "auto" },
+		"rowEnd": { required: false, defaults: "auto" },
+		"colStart": { required: false, defaults: "auto" },
+		"colEnd": { required: false, defaults: "auto" }
+	}
+}
+</script>
+
+<style>
+
+</style>
